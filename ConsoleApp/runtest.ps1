0b4c5897551fb2dbdff249abb98cb3a3f5ca980c
SGrottel <webmaster@sgrottel.de>
2022-05-27 19:06:11 +0200

    Explorer states are not what I expected

--- ConsoleApp/runtest.ps1
+++ ConsoleApp/runtest.ps1
@@ -1,14 +1,18 @@
-param([Parameter(Mandatory)][string]$binPath)
+param(
+    [Parameter(Mandatory)][string]$binPath
+    [Parameter(Mandatory)][string]$repoPath
+    )
 
 Write-Host
 Write-Host "Init:"
 $lines1 = & "$binPath\OpenHereCon.exe"
-Write-Host $lines1
+$lines1
 
 Write-Host
 Write-Host "Starting Explorer #1"
+Write-Host "/E, `"$binPath`""
 $exps = Get-Process -Name '*explorer*' | Where-Object { $_.MainWindowHandle -gt 0 }
-explorer.exe /E, "$binPath"
+explorer.exe "/E," "$binPath"
 for ($i = 0; $i -lt 50; ++$i) {
     $nexps = Get-Process -Name '*explorer*' | Where-Object { $_.MainWindowHandle -gt 0 }
     if ($nexps.length -gt $exps.length) {
@@ -21,12 +25,13 @@ Sleep -Milliseconds 500
 Write-Host
 Write-Host "Detecting:"
 $lines2 = & "$binPath\OpenHereCon.exe"
-Write-Host $lines2
+$lines2
 
 Write-Host
 Write-Host "Starting Explorer #2"
+Write-Host "/SELECT, `"$repoPath\Version.h`""
 $exps = Get-Process -Name '*explorer*' | Where-Object { $_.MainWindowHandle -gt 0 }
-explorer.exe /SELECT, "$binPath\..\..\..\Version.h"
+explorer.exe "/SELECT," "$repoPath\Version.h"
 for ($i = 0; $i -lt 50; ++$i) {
     $nexps = Get-Process -Name '*explorer*' | Where-Object { $_.MainWindowHandle -gt 0 }
     if ($nexps.length -gt $exps.length) {
@@ -39,4 +44,4 @@ Sleep -Milliseconds 500
 Write-Host
 Write-Host "Detecting:"
 $lines3 = & "$binPath\OpenHereCon.exe"
-Write-Host $lines3
+$lines3
