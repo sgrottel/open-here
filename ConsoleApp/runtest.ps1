5c1c92f17017e7369de5de28f2e025d06d9d86e2
SGrottel <webmaster@sgrottel.de>
2022-05-27 18:59:11 +0200

    Test script is now a bit more elaborate on waiting for the explorer windows to be opened

--- ConsoleApp/runtest.ps1
+++ ConsoleApp/runtest.ps1
@@ -1,15 +1,42 @@
 param([Parameter(Mandatory)][string]$binPath)
 
+Write-Host
+Write-Host "Init:"
 $lines1 = & "$binPath\OpenHereCon.exe"
-$lines1
+Write-Host $lines1
 
+Write-Host
+Write-Host "Starting Explorer #1"
+$exps = Get-Process -Name '*explorer*' | Where-Object { $_.MainWindowHandle -gt 0 }
 explorer.exe /E, "$binPath"
+for ($i = 0; $i -lt 50; ++$i) {
+    $nexps = Get-Process -Name '*explorer*' | Where-Object { $_.MainWindowHandle -gt 0 }
+    if ($nexps.length -gt $exps.length) {
+        break;
+    }
+    Sleep -Milliseconds 100
+}
+Sleep -Milliseconds 500
 
+Write-Host
+Write-Host "Detecting:"
 $lines2 = & "$binPath\OpenHereCon.exe"
-$lines2
+Write-Host $lines2
 
+Write-Host
+Write-Host "Starting Explorer #2"
+$exps = Get-Process -Name '*explorer*' | Where-Object { $_.MainWindowHandle -gt 0 }
 explorer.exe /SELECT, "$binPath\..\..\..\Version.h"
+for ($i = 0; $i -lt 50; ++$i) {
+    $nexps = Get-Process -Name '*explorer*' | Where-Object { $_.MainWindowHandle -gt 0 }
+    if ($nexps.length -gt $exps.length) {
+        break;
+    }
+    Sleep -Milliseconds 100
+}
+Sleep -Milliseconds 500
 
+Write-Host
+Write-Host "Detecting:"
 $lines3 = & "$binPath\OpenHereCon.exe"
-$lines3
-
+Write-Host $lines3
