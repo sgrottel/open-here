b328dfdd3f56c86fffc8055829961ba9a4e26ea1
SGrottel <webmaster@sgrottel.de>
2022-05-27 18:48:52 +0200

    Trying to start windows file explorers on CI

--- ConsoleApp/runtest.ps1
+++ ConsoleApp/runtest.ps1
@@ -1,4 +1,15 @@
 param([Parameter(Mandatory)][string]$binPath)
 
-$lines = & "$binPath\OpenHereCon.exe"
-$lines
+$lines1 = & "$binPath\OpenHereCon.exe"
+$lines1
+
+explorer.exe /E, "$binPath"
+
+$lines2 = & "$binPath\OpenHereCon.exe"
+$lines2
+
+explorer.exe /SELECT, "$binPath\..\..\..\Version.h"
+
+$lines3 = & "$binPath\OpenHereCon.exe"
+$lines3
+
